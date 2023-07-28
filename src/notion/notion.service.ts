1cbdf479ce87a4c132831eae7e6e31339e3f9585
Jesica <13519011@std.stei.itb.ac.id>
2023-07-28 15:55:36 +0700

    refactor: notion service

--- src/notion/notion.service.ts
+++ src/notion/notion.service.ts
@@ -31,55 +31,63 @@ export class NotionService {
         'base64',
       );
 
+      const data = {
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri: redirectUri,
+      };
+
+      const headers = {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Basic ${encoded}`,
+      };
+
       const response = await this.httpService.axiosRef.post<
         null,
         INotionAccessToken
-      >(
-        'https://api.notion.com/v1/oauth/token',
-        {
-          grant_type: 'authorization_code',
-          code,
-          redirect_uri: redirectUri,
-        },
-        {
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            Authorization: `Basic ${encoded}`,
-          },
-        },
-      );
+      >('https://api.notion.com/v1/oauth/token', data, {
+        headers,
+      });
 
       // get the access token
       const tokenInfo = response.data;
 
       // update or store notion token info on db
+      const notionData = {
+        accessToken: tokenInfo.access_token,
+        botId: tokenInfo.bot_id,
+        duplicatedTemplateId: tokenInfo.duplicate_template_id,
+        tokenType: tokenInfo.token_type,
+        workspaceId: tokenInfo.workspace_id,
+        workspaceName: tokenInfo.workspace_name,
+        userId: user.id,
+      };
+
       await this.prisma.notion.upsert({
         where: { userId: user.id },
-        update: {
-          accessToken: tokenInfo.access_token,
-          botId: tokenInfo.bot_id,
-          duplicatedTemplateId: tokenInfo.duplicate_template_id,
-          tokenType: tokenInfo.token_type,
-          workspaceId: tokenInfo.workspace_id,
-          workspaceName: tokenInfo.workspace_name,
-        },
-        create: {
-          accessToken: tokenInfo.access_token,
-          botId: tokenInfo.bot_id,
-          duplicatedTemplateId: tokenInfo.duplicate_template_id,
-          tokenType: tokenInfo.token_type,
-          workspaceId: tokenInfo.workspace_id,
-          workspaceName: tokenInfo.workspace_name,
-          userId: user.id,
-        },
+        create: notionData,
+        update: notionData,
       });
 
-      return { access_token: response.data.access_token };
+      return { access_token: tokenInfo.access_token };
     } catch (error) {
       throw new ForbiddenException(
         error.response.data.error_description as string,
       );
     }
+  }
+
+  async getDatabases(@GetUser() user: User) {
+    const { accessToken } = await this.prisma.notion.findUnique({
+      where: { userId: user.id },
+      select: { accessToken: true },
+    });
+    const payload = { page_size: 100 };
+    const headers = {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    };
   }
 }
