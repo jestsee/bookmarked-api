405a41aa734c8cf6350fb6e9360850b63c31e88d
Jesica <13519011@std.stei.itb.ac.id>
2023-07-28 10:50:16 +0700

    feat: update notion integration

--- src/notion/notion.controller.ts
+++ src/notion/notion.controller.ts
@@ -1,13 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { NotionService } from './notion.service';
 import { NotionIntegrationDto } from './dto/notion-integration.dto';
+import { JwtGuard } from 'src/auth/guards';
+import { GetUser } from 'src/auth/decorator';
+import { User } from '@prisma/client';
 
+@UseGuards(JwtGuard)
 @Controller('notion')
 export class NotionController {
   constructor(private notionService: NotionService) {}
 
   @Post('integration')
-  integration(@Body() dto: NotionIntegrationDto) {
-    return this.notionService.getAccessToken(dto);
+  integration(@Body() dto: NotionIntegrationDto, @GetUser() user: User) {
+    console.log({ hai: user });
+    return this.notionService.getAccessToken(dto, user);
   }
 }
--- src/notion/notion.service.ts
+++ src/notion/notion.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { NotionIntegrationDto } from './dto/notion-integration.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ConfigService } from '@nestjs/config';
 import { HttpService } from '@nestjs/axios';
+import { GetUser } from 'src/auth/decorator';
+import { User } from '@prisma/client';
 
 // https://developers.notion.com/docs/authorization#prompt-for-a-standard-integration-with-no-template-option-default
 @Injectable()
@@ -12,7 +14,10 @@ export class NotionService {
     private config: ConfigService,
     private readonly httpService: HttpService,
   ) {}
-  async getAccessToken({ code }: NotionIntegrationDto): Promise<{
+  async getAccessToken(
+    { code }: NotionIntegrationDto,
+    @GetUser() user: User,
+  ): Promise<{
     access_token: string;
   }> {
     try {
@@ -20,18 +25,14 @@ export class NotionService {
       const clientSecret = this.config.get('NOTION_OAUTH_CLIENT_SECRET');
       const redirectUri = this.config.get('NOTION_REDIRECT_URI');
 
-      console.log({ clientId, clientSecret });
-
       // encode in base 64
       const encoded = Buffer.from(`${clientId}:${clientSecret}`).toString(
         'base64',
       );
 
       const response = await this.httpService.axiosRef.post<
-        string,
-        {
-          access_token: string;
-        }
+        null,
+        { data: { access_token: string } }
       >(
         'https://api.notion.com/v1/oauth/token',
         {
@@ -48,22 +49,20 @@ export class NotionService {
         },
       );
 
-      console.log({ response });
-      return response;
+      // get the access token
+      const accessToken = response.data.access_token;
+
+      // update access token on user data
+      await this.prisma.user.update({
+        where: { id: user.id },
+        data: { notionAccessToken: accessToken },
+      });
+
+      return { access_token: response.data.access_token };
     } catch (error) {
-      console.error({ error: error.response.data });
+      throw new ForbiddenException(
+        error.response.data.error_description as string,
+      );
     }
   }
-  // TODO still error
-  // example for success response from notion API
-  // data: {
-  //   access_token: 'secret_onpN4l4al4fdT5heFtgcR48FEofReRZO6nX9t1RXSTz',
-  //   token_type: 'bearer',
-  //   bot_id: '3e2aeee7-c14b-4295-b834-febdccbfb6a1',
-  //   workspace_name: "Jesica's Notion",
-  //   workspace_icon: 'https://lh3.googleusercontent.com/a-/AOh14GiOJxym9rV9Qc_G78OgM712SU6koYEYdzZbAnMK=s100',
-  //   workspace_id: '1e66ba03-a88e-411e-aef0-1b9581be3f9f',
-  //   owner: [Object],
-  //   duplicated_template_id: null
-  // }
 }
