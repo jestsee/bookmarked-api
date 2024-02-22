84310c9abddcdeb8867be045c38aa83568a1c26f
Jesica <13519011@std.stei.itb.ac.id>
2024-02-22 19:27:56 +0700

    chore: remove unnecessary import & update Dockerfile

--- src/notion/notion.service.ts
+++ src/notion/notion.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { NotionIntegrationDto } from './dto/notion-integration.dto';
 import { NotionSdkService } from 'src/notion-sdk/notion-sdk.service';
 import { TwitterDataType } from 'src/twitter/dto';
